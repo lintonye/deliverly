fd3ad9b7eb54c9ba1ae798cf63b4ad0cf2fd0440
Linton Ye <linton@jimulabs.com>
2015-11-14 11:48:24 -0800

    Initial commit: mapbox works

---
+++ index.ios.js
@@ -0,0 +1,219 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+'use strict';
+
+var React = require('react-native');
+var Mapbox = require('react-native-mapbox-gl');
+var {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  MapView,
+  TouchableOpacity,
+  SliderIOS,
+} = React;
+
+// var { Icon, } = require('react-native-icons');
+// var Popover = require('react-native-popover');
+
+// TODO extract common code of filters?
+
+class DestinationsFilter extends View {
+  render() {
+    return (
+      <View style={filterStyles.filterContainer}>
+        <Text style={filterStyles.filterTitle}>Destinations</Text>
+        <Text style={filterStyles.filterValue} ref='valueLabel'>All</Text>
+      </View>
+    )
+  }
+}
+
+// class BudgetFilter extends View {
+var BudgetFilter = React.createClass({
+  onSliderChange(value) {
+    this.setState({ value })
+  },
+  computeValueLabel(value) {
+    return value == 0 ? 'Any budget' : `Above $${value}`
+  },
+  getInitialState() {
+    return {
+      value: 0,
+    }
+  },
+  render() {
+    return (
+      <View style={filterStyles.filterContainer}>
+        <Text style={filterStyles.filterTitle}>Budget</Text>
+        <Text style={filterStyles.filterValue} ref='valueLabel'>{this.computeValueLabel(this.state.value)}</Text>
+        <SliderIOS
+          style={filterStyles.slider}
+          value={0}
+          maximumValue={100}
+          step={10}
+          onValueChange={this.onSliderChange}
+          />
+      </View>
+    )
+  }
+}
+)
+
+var FilterPopover = React.createClass({
+  render() {
+    return (
+      <View
+        style={this.style()}
+        isVisible={this.props.isVisible}
+        fromRect={this.props.fromRect}>
+        <DestinationsFilter />
+        <BudgetFilter />
+      </View>
+    )
+  },
+  style() {
+    return {
+      position: 'absolute',
+      top: 40,
+      left: 10,
+    };
+  }
+});
+
+var filterStyles = StyleSheet.create({
+  filterContainer: {
+  },
+  filterTitle: {
+    fontSize: 15
+  },
+  filterValue: {
+    fontSize: 10
+  },
+  slider: {
+    width: 200,
+  },
+});
+
+var deliverly = React.createClass({
+  showFilterPopover() {
+    this.refs.btFilter.measure((ox, oy, width, height, px, py) => {
+      this.setState({
+        filterPopover: {
+          isVisible: true,
+          buttonRect: {x: px, y: py, width: width, height: height}
+        },
+      });
+    });
+  },
+
+  closeFilterPopover() {
+    setState({
+      filterPopover: { isVisible: false }
+    })
+  },
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.toolbar}>
+          <TouchableOpacity>
+            <Text
+              name='fontawesome|crosshairs'
+              size={20}
+              color='#333333'
+              style={styles.toolbarButton}
+              />
+          </TouchableOpacity>
+          <Text style={styles.toolbarTitle}>Deliverly</Text>
+          <TouchableOpacity ref='btFilter' onPress={this.showFilterPopover}>
+            <Text
+              name='fontawesome|filter'
+              size={20}
+              color='#333333'
+              style={styles.toolbarButton}
+              />
+          </TouchableOpacity>
+        </View>
+        <Mapbox style={styles.map}
+          zoomEnabled={true}
+          annotations={this.state.annotations}
+          direction={0}
+          accessToken="sk.eyJ1IjoiamltdWxhYnMiLCJhIjoiY2lnd2w1Y2F3MHQ0cnd5bTBtYm1mNzFlbSJ9.hJTKysrSX6M55PD_W-vs-w"
+          centerCoordinate={this.state.center}
+          zoomLevel={this.state.zoom}
+          styleURL={'asset://styles/streets-v8.json'}
+          />
+        <FilterPopover
+          isVisible={this.state.filterPopover.isVisible}
+          fromRect={this.state.filterPopover.buttonRect}
+          onClose={this.closePopover}
+          />
+      </View>
+    );
+  },
+
+  getInitialState() {
+    return {
+      position: 'Unknown',
+      annotations: [
+        {coordinates: [37.33756603, -122.04120235],
+          type: 'point',
+          title: 'Haha', subtitle: 'Superman1', hasLeftCallout: false, hasRightCallout: true},
+        {coordinates: [37.32756608, -122.04120245],
+          type: 'point',
+          title: 'Haha', subtitle: 'Superman2', hasLeftCallout: true, hasRightCallout: true}
+      ],
+      center: { latitude: 37.33756603, longitude: -122.04120235 },
+      zoom: 13,
+      filterPopover: {
+        isVisible: true,
+        buttonRect: {x: 10, y: 80, width: 10, height: 10}
+      }
+    };
+  },
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      (position) => this.setState({position}),
+      (error) => console.log(error.message),
+      {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000}
+    );
+  },
+
+  componentWillUnmount() {
+  },
+});
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  toolbar: {
+    flexDirection: 'row',
+    backgroundColor: '#81c04d',
+    paddingTop: 30,
+    paddingBottom: 10,
+  },
+  toolbarTitle: {
+    textAlign: "center",
+    marginTop: 5,
+    flex: 1,
+    fontSize: 18,
+  },
+  toolbarButton: {
+    width: 40,
+    height: 30,
+  },
+  map: {
+    flex: 1,
+    width: 400,
+    borderWidth: 1,
+    borderColor: '#000000',
+  },
+});
+
+AppRegistry.registerComponent('deliverly', () => deliverly);
